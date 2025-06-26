6cb7bc689c43c1299fcf4c2e7674aadee6d48745
Kauan Oliveria Freitas <kauanffreitas57@gmail.com>
2025-06-25 21:42:06 -0300

    Finalizando o laboratorio

--- ScriptsWorkbench/consulta-sobre-multiplas-relacoes2.sql
+++ ScriptsWorkbench/consulta-sobre-multiplas-relacoes2.sql
@@ -24,9 +24,7 @@ CREATE TABLE Curso (
 id INT PRIMARY KEY AUTO_INCREMENT,
 nome VARCHAR(100) NOT NULL UNIQUE,
 id_departamento INT NOT NULL,
-id_campus INT NOT NULL,
-FOREIGN KEY (id_departamento) REFERENCES Departamento (id),
-FOREIGN KEY (id_campus) REFERENCES Campus (id));
+FOREIGN KEY (id_departamento) REFERENCES Departamento (id));
 
 DESC Curso;
 
@@ -34,11 +32,7 @@ CREATE TABLE Aluno (
 id INT PRIMARY KEY AUTO_INCREMENT,
 nome VARCHAR(100) NOT NULL UNIQUE,
 id_curso INT NOT NULL,
-id_departamento INT NOT NULL,
-id_campus INT NOT NULL,
-FOREIGN KEY (id_curso) REFERENCES Curso (id),
-FOREIGN KEY (id_departamento) REFERENCES Departamento (id),
-FOREIGN KEY (id_campus) REFERENCES Campus (id));
+FOREIGN KEY (id_curso) REFERENCES Curso (id));
 
 DESC Aluno;
 
@@ -54,48 +48,146 @@ SELECT * FROM Departamento;
 
 SELECT C.nome, D.nome FROM Departamento D INNER JOIN Campus C ON D.id_campus = C.id;
 
-INSERT INTO Curso (nome, id_departamento, id_campus) 
-VALUES ('Engenharia de Telecomunicacoes', 1, 1), 
-('Analise e Desenvolvimento de Sistemas', 1, 1), 
-('Engenharia Elétrica ', 3, 2),
-('Ciências da Computação', 4, 3);
+INSERT INTO Curso (nome, id_departamento) 
+VALUES ('Engenharia de Telecomunicacoes', 1), 
+('Analise e Desenvolvimento de Sistemas', 1), 
+('Engenharia Elétrica ', 3),
+('Ciências da Computação', 4);
 
 SELECT C.nome AS Curso, D.nome AS Departamento, Ca.nome AS Campus FROM Curso C 
 INNER JOIN Departamento D ON C.id_departamento = D.id 
-INNER JOIN Campus Ca ON C.id_campus = Ca.id;
+INNER JOIN Campus Ca ON D.id_campus = Ca.id;
 
-INSERT INTO Aluno (nome, id_curso, id_departamento, id_campus) VALUES
-('Marcos', 1, 1, 1),
-('Maria', 1, 1, 1),
-('Jorge', 1, 1, 1),
-('Roberto', 1, 1, 1),
-('Ana', 1, 1, 1),
-('Kauan', 2, 1, 1),
-('Thays', 2, 1, 1),
-('Lucas', 2, 1, 1),
-('Fernanda', 2, 1, 1),
-('Gustavo', 3, 3, 2),
-('Natan', 3, 3, 2),
-('Pedro', 4, 4, 3);
+INSERT INTO Aluno (nome, id_curso) VALUES
+('Marcos', 1),
+('Maria', 1),
+('Jorge', 1),
+('Roberto', 1),
+('Ana', 1),
+('Kauan', 2),
+('Thays', 2),
+('Lucas', 2),
+('Fernanda', 2),
+('Gustavo', 3),
+('Natan', 3),
+('Pedro', 4);
 
 SELECT A.nome AS Aluno, C.nome AS Curso, D.nome AS Departamento, Ca.nome AS Campus FROM Aluno A 
 INNER JOIN Curso C ON A.id_curso = C.id
-INNER JOIN Departamento D ON A.id_departamento = D.id 
-INNER JOIN Campus Ca ON A.id_campus = Ca.id;
+INNER JOIN Departamento D ON C.id_departamento = D.id 
+INNER JOIN Campus Ca ON D.id_campus = Ca.id;
 
 -- Questão 19
-SELECT C.nome AS Curso, Ca.nome AS Campus FROM Curso C 
-INNER JOIN Campus Ca ON C.id_campus = Ca.id;
+SELECT C.nome AS Curso, Ca.nome AS Campus FROM Curso C
+INNER JOIN Departamento D ON C.id_departamento = D.id 
+INNER JOIN Campus Ca ON D.id_campus = Ca.id;
 
 -- Questão 20
-SELECT A.nome AS Aluno FROM Aluno A 
-INNER JOIN Campus Ca ON A.id_campus = Ca.id
+SELECT A.nome AS Aluno FROM Aluno A
+INNER JOIN Curso C ON A.id_curso = C.id
+INNER JOIN Departamento D ON C.id_departamento = D.id 
+INNER JOIN Campus Ca ON D.id_campus = Ca.id
 WHERE Ca.nome = 'São José';
 
 -- Questão 21
 SELECT Ca.nome AS Campus, D.nome AS Departamento, C.nome AS Curso, COUNT(A.id) AS TotalAlunos FROM Aluno A 
 INNER JOIN Curso C ON A.id_curso = C.id
-INNER JOIN Departamento D ON A.id_departamento = D.id 
-INNER JOIN Campus Ca ON A.id_campus = Ca.id
-GROUP BY Ca.nome;
+INNER JOIN Departamento D ON C.id_departamento = D.id 
+INNER JOIN Campus Ca ON D.id_campus = Ca.id
+GROUP BY Ca.nome, D.nome, C.nome;
 
+CREATE DATABASE IF NOT EXISTS empresarial;
+USE empresarial;
+
+DROP TABLE Dependente;
+DROP TABLE Empregado;
+DROP TABLE Empresa;
+DROP TABLE Grupo;
+
+CREATE TABLE Grupo (
+id INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(100) NOT NULL UNIQUE);
+
+DESC Grupo;
+
+CREATE TABLE Empresa (
+id INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(100) NOT NULL UNIQUE,
+id_grupo INT NOT NULL,
+FOREIGN KEY (id_grupo) REFERENCES Grupo (id));
+
+DESC Empresa;
+
+CREATE TABLE Empregado (
+id INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(100) NOT NULL UNIQUE,
+id_empresa INT NOT NULL,
+FOREIGN KEY (id_empresa) REFERENCES Empresa (id));
+
+DESC Empregado;
+
+CREATE TABLE Dependente (
+id INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(100) NOT NULL UNIQUE,
+id_empregado INT NOT NULL,
+FOREIGN KEY (id_empregado) REFERENCES Empregado (id));
+
+DESC Dependente;
+
+INSERT INTO Grupo (nome) 
+VALUES ('ABC'), ('DEF'), ('GHI');
+
+SELECT * FROM Grupo;
+
+INSERT INTO Empresa (nome, id_grupo) 
+VALUES ('O baratão',1), ('A moda', 1), ('Popular', 2), ('Exclusiva', 3);
+
+SELECT * FROM Empresa;
+
+SELECT G.nome, E.nome FROM Grupo G INNER JOIN Empresa E ON G.id = E.id_grupo;
+
+INSERT INTO Empregado (nome, id_empresa) 
+VALUES ('Sofia', 1), 
+('Ana', 1), 
+('Adriano', 2),
+('Kauan', 3),
+('Thays', 4);
+
+SELECT * FROM Empregado;
+
+INSERT INTO Dependente (nome, id_empregado) VALUES
+('Julia', 1),
+('Carmelita', 1),
+('Guilherme', 1),
+('Jorge', 1),
+('Firmino', 1),
+('Magali', 3),
+('Chico Bento', 3),
+('Cascão', 3),
+('Cebolinha', 3),
+('Livinha', 4),
+('Luciene', 4),
+('Sheik', 5);
+
+SELECT * FROM Dependente;
+
+-- Questão 22
+
+SELECT G.nome AS Grupo, E.nome AS Empregado FROM Empregado E
+INNER JOIN Empresa Emp ON E.id_empresa = Emp.id
+INNER JOIN Grupo G ON Emp.id_grupo = G.id;
+
+-- Questão 23
+
+SELECT D.nome FROM Dependente D
+INNER JOIN Empregado E ON D.id_empregado = E.id
+INNER JOIN Empresa Emp ON E.id_empresa = Emp.id
+INNER JOIN Grupo G ON Emp.id_grupo = G.id
+WHERE G.nome = 'ABC';
+
+-- Questão 24
+SELECT G.nome AS Grupo, Emp.nome AS Empresa, E.nome AS Empregado, COUNT(D.id) AS TotalDependentes FROM Dependente D
+INNER JOIN Empregado E ON D.id_empregado = E.id
+INNER JOIN Empresa Emp ON E.id_empresa = Emp.id
+INNER JOIN Grupo G ON Emp.id_grupo = G.id
+GROUP BY G.nome, Emp.nome, E.nome;
